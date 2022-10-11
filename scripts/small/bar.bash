5789703962cef01046752db2e2971b7a1cfea97a
Georgi Tarashev <georgitarashev@gmail.com>
2022-10-11 21:08:58 +0300

    made bar.bash distro agnostic + other small changes

--- scripts/small/bar.bash
+++ scripts/small/bar.bash
@@ -1,29 +1,40 @@
 #!/bin/bash
 counter=0
+# create the log file variable
+logfile="$HOME/.config/net/logfile"
+
+# continous loop
 while :; do
+    # update some things only once a min rather than once a second
     if [[ $(expr $counter % 60) == 0 ]] ; then
-	curr_date="$(date +'%A (%-e/%-m/%Y)')"
-	curr_time="$(date +'%-l:%M %p')"
-	battery="$(cat /sys/class/power_supply/BAT1/capacity)"
-	counter=0
+        # get the date and the time (see `man date`)
+	    curr_date="$(date +'%A (%-e/%-m/%Y)')"
+	    curr_time="$(date +'%-l:%M %p')"
+	    # get the battery percentage (see `man upower` then grep and awk to format)
+	    battery="$(upower -i `upower -e | grep 'BAT'` | grep 'percentage' | awk '{ print $2 }')"
+	    # reset counter
+	    counter=0
     fi
     
-
-    # networking
-    logfile="$HOME/.config/net/logfile"
-    # get current
-    rxcurrent="$(cat /sys/class/net/wlp2s0/statistics/rx_bytes)"
-    txcurrent="$(cat /sys/class/net/wlp2s0/statistics/tx_bytes)"
+    # get the current down and up bytes (total since start of session) 
+    rxcurrent="$(cat /proc/net/dev | grep 'wlo1' | awk '{ print $2 }')"
+    txcurrent="$(cat /proc/net/dev | grep 'wlo1' | awk '{ print $10 }')"
+    # get the old data
     prevdata="$(cat "$logfile")"
 
-    # get up diff
-    net="$(printf "🔻 %sKib/s 🔺 %sKib/s" \
+    # get the difference and format into Kib/s
+    net="$(printf "DOWN: %sKib/s UP: %sKib/s" \
     "$(((rxcurrent-${prevdata%% *})/1024))" \
     "$(((txcurrent-${prevdata##* })/1024))")"
     
+    # update the log file 
     echo "$rxcurrent $txcurrent" > "$logfile"
 
-    xsetroot -d "$DISPLAY" -name "$net | 🔋 $battery% | 📅 $curr_date | ⏰ $curr_time"
+    # set xsetroot name ($DISPLAY is an environment variable declared by the system when
+    # an xsession is started)
+    xsetroot -d "$DISPLAY" -name "$net | BAT: $battery | $curr_date $curr_time"
+    # update counter
     counter=$(expr $counter + 1)
-    sleep 2s
+    # sleep for a second (no need to update more often)
+    sleep 1s
 done
