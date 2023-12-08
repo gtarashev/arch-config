b36cd6590b1326b717810af4483d93d61ca2c929
Georgi Tarashev <georgitarashev@gmail.com>
2023-12-08 14:05:11 +0200

    some updates

--- scripts/small/bar.bash
+++ scripts/small/bar.bash
@@ -1,8 +1,5 @@
 #!/bin/bash
 counter=0
-# create the log file variable
-logfile="$HOME/.config/net/logfile"
-echo "0 0" > $logfile
 
 # continous loop
 while :; do
@@ -13,29 +10,26 @@ while :; do
 	    curr_time="$(date +'%-l:%M %p')"
 	    # get the battery percentage (see `man upower` then grep and awk to format)
 	    battery="$(upower -i `upower -e | grep 'BAT'` | grep 'percentage' | awk '{ print $2 }')"
+    fi
+
+    # update once every 15 minutes (do not want to constantly send curl requests to wttr.in)
+    if [[ $(expr $counter % 900) == 0 ]] ; then
+      weather="$(curl -s wttr.in/burgas?format='%c+%t+(%f)')"
 	    # reset counter
 	    counter=0
     fi
-    
-    # get the current down and up bytes (total since start of session) 
-    rxcurrent="$(cat /proc/net/dev | grep 'wlo1' | awk '{ print $2 }')"
-    txcurrent="$(cat /proc/net/dev | grep 'wlo1' | awk '{ print $10 }')"
-    # get the old data
-    prevdata="$(cat "$logfile")"
 
-    # get the difference and format into Kib/s
-    net="$(printf "DOWN: %sKib/s UP: %sKib/s" \
-    "$(((rxcurrent-${prevdata%% *})/1024))" \
-    "$(((txcurrent-${prevdata##* })/1024))")"
     
-    # update the log file 
-    echo "$rxcurrent $txcurrent" > "$logfile"
-
     # set xsetroot name ($DISPLAY is an environment variable declared by the system when
     # an xsession is started)
-    xsetroot -d "$DISPLAY" -name "$net | BAT: $battery | $curr_date  $curr_time"
+    printing="$weather | 🔋 $battery | $curr_date  🕰 $curr_time"
+    xsetroot -d "$DISPLAY" -name "$printing"
     # update counter
     counter=$(expr $counter + 1)
     # sleep for a second (no need to update more often)
     sleep 1s
 done
+
+
+
+
--- scripts/small/wpaper.sh
+++ scripts/small/wpaper.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # change current wallpaper and make it default
 f="$(readlink -f $1)" &&
-xwallpaper --zoom $f &&
+feh --bg-scale $f &&
 echo "Wallpaper changed." &&
 ln -fLs $f $HOME/pictures/wallpapers/wallpaper &&
 echo "Image set to default."
